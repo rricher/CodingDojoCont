afd713b1ba368c173934989c6db2a8a221097f95
rricher <ryanricher@comcast.net>
2020-03-18 19:06:12 -0700

    proj 1

--- JavaScript/mern/my-project/server.js
+++ JavaScript/mern/my-project/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-
+app.use(cors());
 require('./server/routes/person.routes')(app);
 
 app.listen(8000, () =>  {
